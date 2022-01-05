689ef23b060b50e704b59ac43eec1f1a4130fea3
Takumi <nase49946@gmail.com>
2022-01-05 16:00:04 +0900

    googleマップ表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
 function App() {
   return (
-    <div>
-      <p>hello world!!</p>
+    <div className="max-w-4xl mx-auto">
+      <LoadScript googleMapsApiKey="AIzaSyB5KlhSNJ37deePhGn1Can7L1uK0MaFT_M">
+        <GoogleMap
+          mapContainerStyle={{ width: '100%', height: '400px' }}
+          center={{ lat: 38, lng: 137.5936 }}
+          zoom={5}
+        ></GoogleMap>
+      </LoadScript>
     </div>
   );
 }
