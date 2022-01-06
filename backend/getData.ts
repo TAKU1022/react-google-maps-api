7aa36b2d251bdf33cbaf4d1a41570ebf4befb4e8
Takumi <nase49946@gmail.com>
2022-01-06 19:28:20 +0900

    エリアデータのjsonファイル書き出し

--- backend/getData.ts
+++ backend/getData.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import axios from 'axios';
 require('dotenv').config();
 
-const getData = async () => {
+const getLargeArea = async () => {
   const res = await axios.get(
     `http://webservice.recruit.co.jp/hotpepper/large_area/v1/`,
     {
@@ -12,8 +12,23 @@ const getData = async () => {
       },
     }
   );
-
   fs.writeFileSync('./src/data/largeArea.json', JSON.stringify(res.data));
   console.log('finish load largeArea data');
 };
-getData();
+
+const getMiddleArea = async () => {
+  const res = await axios.get(
+    `http://webservice.recruit.co.jp/hotpepper/middle_area/v1/`,
+    {
+      params: {
+        key: process.env.REACT_APP_HOTPEPPER_API_KEY,
+        format: 'json',
+      },
+    }
+  );
+  fs.writeFileSync('./src/data/middleArea.json', JSON.stringify(res.data));
+  console.log('finish load middleArea data');
+};
+
+getLargeArea();
+getMiddleArea();
