8852cfb25aae36dcb1a833ca0b0880058b3e719a
Takumi <nase49946@gmail.com>
2022-01-17 16:36:23 +0900

    provider作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { GourmetFormProvider } from './contexts/GourmetFormProvider';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GourmetFormProvider>
+      <App />
+    </GourmetFormProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
