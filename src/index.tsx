1b4135eccc0e837b33212b7efdfd34e42ed58f6b
Takumi <nase49946@gmail.com>
2022-01-18 16:17:24 +0900

    MapLoadingProvider追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { GourmetFormProvider } from './contexts/GourmetFormProvider';
+import { MapLoadingProvider } from './contexts/MapLoadingProvider';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GourmetFormProvider>
-      <App />
-    </GourmetFormProvider>
+    <MapLoadingProvider>
+      <GourmetFormProvider>
+        <App />
+      </GourmetFormProvider>
+    </MapLoadingProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
