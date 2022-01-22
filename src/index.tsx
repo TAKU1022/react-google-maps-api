64fb1cb9acbd5aed26b1f53fd016b0234b1a748f
Takumi <nase49946@gmail.com>
2022-01-22 16:57:44 +0900

    不要コード削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { GourmetFormProvider } from './contexts/GourmetForm/GourmetFormProvider';
 import { MapLoadingProvider } from './contexts/MapLoading/MapLoadingProvider';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <MapLoadingProvider>
-      <GourmetFormProvider>
-        <App />
-      </GourmetFormProvider>
+      <App />
     </MapLoadingProvider>
   </React.StrictMode>,
   document.getElementById('root')
